1a6d14edf31ab2ede77c01bf544f351a91c8ae00
duniyagwan <duniyagwan@gmail.com>
2012-06-28 19:44:42 -0500

    made changes

--- mtm1526/assignment-4/js/planets.js
+++ mtm1526/assignment-4/js/planets.js
@@ -1,14 +1,27 @@
 $(document).ready(function(){
-	$.get('planets', function(data){
-});
-	
-$('#gethtml').on('click', function(){
-	$('#fillme').load('load-me.html');
-});
-	
-$('#sendme').on('change', function(){
-	$.post('save.php', {
-		sendme : $('#sendme').val()
-		});
+
+	$.get('earth.html', function(data){
+		$('div').prepend(data);
 	});
+	$('#earth').on('click', function(){
+		$('#fillme').load('earth.html');
+		$('nav li').removeClass('current');
+		$('#earth').addClass('current');
+	});
+	$('#mars').on('click', function(){
+		$('#fillme').load('mars.html');
+		$('nav li').removeClass('current');
+		$('#mars').addClass('current');
+	});
+	$('#jupiter').on('click', function(){
+		$('#fillme').load('jupiter.html');
+		$('nav li').removeClass('current');
+		$('#jupiter').addClass('current');
+	});
+	$('#venus').on('click', function(){
+		$('#fillme').load('venus.html');
+		$('nav li').removeClass('current');
+		$('#venus').addClass('current');
+	});
+
 });
